f43e6ad5ff4968cf049eb8718fa5b36a4617206e
Alexis Huerta <alehuer90@gmail.com>
2019-05-07 17:23:54 -0700

    rearranged class members

--- cs146rbtrees/src/cs146S19/huerta/project4/RedBlackTree.java
+++ cs146rbtrees/src/cs146S19/huerta/project4/RedBlackTree.java
@@ -3,54 +3,9 @@ package cs146S19.huerta.project4;
 public class RedBlackTree<Key extends Comparable<Key>> {
 	private static RedBlackTree.Node<String> root;
 
-	//----------Node Class----------
-	public static class Node<Key extends Comparable<Key>> { // changed to static
-
-		Key key;
-		Node<String> parent;
-		Node<String> leftChild;
-		Node<String> rightChild;
-		boolean isRed;
-		int color;
-
-		/**
-		 * Constructor for a Node
-		 * @param data - data to be held by the Node
-		 */
-		public Node(Key data) {
-			this.key = data;
-			leftChild = null;
-			rightChild = null;
-		}
-
-		/**
-		 * Comparing the data inside two Nodes
-		 * @param n - node to be compared to
-		 * @return - negative if this is before, zero if this and n are the same, and positive if this comes after
-		 */
-		public int compareTo(Node<Key> n) { // this < that <0
-			return key.compareTo(n.key); // this > that >0
-		}
-
-		/**
-		 * Determines if this node is a leaf
-		 * @return - true if node is a leaf
-		 */
-		public boolean isLeaf() {
-			if (this.equals(root) && this.leftChild == null && this.rightChild == null)
-				return true;
-			if (this.equals(root))
-				return false;
-			if (this.leftChild == null && this.rightChild == null) {
-				return true;
-			}
-			return false;
-		}
-	}
-	//----------End Node Class----------
-
 	/**
 	 * Checks if a specified node is a leaf
+	 * 
 	 * @param n - the node to be checked
 	 * @return - true if the node is a leaf
 	 */
@@ -65,25 +20,6 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 		return false;
 	}
 
-	//----------Visitor Interface that defines visit()----------
-	public interface Visitor<Key extends Comparable<Key>> {
-		/**
-		 * This method is called at each node.
-		 * 
-		 * @param n - the visited node
-		 */
-		void visit(Node<Key> n);
-	}
-	//----------End Visitor Interface----------
-	
-	/**
-	 * Prints out the key for given node
-	 * @param n - node visited
-	 */
-	public void visit(Node<Key> n) {
-		System.out.println(n.key);
-	}
-
 	/**
 	 * Initial call to Visits the tree in pre-order
 	 */
@@ -94,6 +30,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * Recursive visit and printing of the tree in Preorder
+	 * 
 	 * @param node - the node to start with
 	 */
 	public void printTree(RedBlackTree.Node<String> node) {
@@ -108,6 +45,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 	// place a new node in the RB tree with data the parameter and color it red.
 	/**
 	 * Creating a new node with the specified data and adding it to the tree.
+	 * 
 	 * @param data - data to be added to the tree
 	 */
 	public void addNode(String data) { // this < that <0. this > that >0
@@ -117,6 +55,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * Calls addNode to insert a node
+	 * 
 	 * @param data - data to insert
 	 */
 	public void insert(String data) {
@@ -125,6 +64,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * LookUp a node whose data matches the specified String
+	 * 
 	 * @param k - string to be looked up
 	 * @return - the node that contains the specified String
 	 */
@@ -135,6 +75,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * Finds the sibling of a specified node
+	 * 
 	 * @param n - the node to be used to get the sibling
 	 * @return - the sibling node
 	 */
@@ -145,6 +86,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * Finds the aunt of a specified node
+	 * 
 	 * @param n - the node to be used to get the aunt
 	 * @return - the aunt node
 	 */
@@ -155,6 +97,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * Finds the grandparent of a specified node
+	 * 
 	 * @param n - the node to be used to get the grandparent
 	 * @return - the grandparent node
 	 */
@@ -164,6 +107,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * Rotate left at the specified node
+	 * 
 	 * @param n - node to rotate left
 	 */
 	public void rotateLeft(RedBlackTree.Node<String> n) {
@@ -172,6 +116,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * Rotate right at the specified node
+	 * 
 	 * @param n - node to rotate right
 	 */
 	public void rotateRight(RedBlackTree.Node<String> n) {
@@ -180,6 +125,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * Recursive method used to traverse the tree to make it a RBT (BIG)
+	 * 
 	 * @param current - the current pointer node
 	 */
 	public void fixTree(RedBlackTree.Node<String> current) {
@@ -188,6 +134,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * Checks to see if a node does not contain a key
+	 * 
 	 * @param n - the node to be checked
 	 * @return - true if empty
 	 */
@@ -199,9 +146,11 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 	}
 
 	/**
-	 * Compares two nodes and determines if the child node is to the left of the specified parent node
+	 * Compares two nodes and determines if the child node is to the left of the
+	 * specified parent node
+	 * 
 	 * @param parent - the parent node
-	 * @param child - the child node
+	 * @param child  - the child node
 	 * @return - true if the child is a left child
 	 */
 	public boolean isLeftChild(RedBlackTree.Node<String> parent, RedBlackTree.Node<String> child) {
@@ -211,8 +160,79 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 		return false;
 	}
 
+	// ----------Node Class----------
+	public static class Node<Key extends Comparable<Key>> { // changed to static
+
+		Key key;
+		Node<String> parent;
+		Node<String> leftChild;
+		Node<String> rightChild;
+		boolean isRed;
+		int color;
+
+		/**
+		 * Constructor for a Node
+		 * 
+		 * @param data - data to be held by the Node
+		 */
+		public Node(Key data) {
+			this.key = data;
+			leftChild = null;
+			rightChild = null;
+		}
+
+		/**
+		 * Comparing the data inside two Nodes
+		 * 
+		 * @param n - node to be compared to
+		 * @return - negative if this is before, zero if this and n are the same, and
+		 *         positive if this comes after
+		 */
+		public int compareTo(Node<Key> n) { // this < that <0
+			return key.compareTo(n.key); // this > that >0
+		}
+
+		/**
+		 * Determines if this node is a leaf
+		 * 
+		 * @return - true if node is a leaf
+		 */
+		public boolean isLeaf() {
+			if (this.equals(root) && this.leftChild == null && this.rightChild == null)
+				return true;
+			if (this.equals(root))
+				return false;
+			if (this.leftChild == null && this.rightChild == null) {
+				return true;
+			}
+			return false;
+		}
+	}
+	// ----------End Node Class----------
+
+	// ----------Visitor Interface that defines visit()----------
+	public interface Visitor<Key extends Comparable<Key>> {
+		/**
+		 * This method is called at each node.
+		 * 
+		 * @param n - the visited node
+		 */
+		void visit(Node<Key> n);
+	}
+	// ----------End Visitor Interface----------
+
+	/**
+	 * Prints out the key for given node
+	 * 
+	 * @param n - node visited
+	 */
+	public void visit(Node<Key> n) {
+		System.out.println(n.key);
+	}
+
 	/**
 	 * Executes a pre-order visit of the tree starting at the root
+	 * 
 	 * @param v
 	 */
 	public void preOrderVisit(Visitor<String> v) {
@@ -221,6 +241,7 @@ public class RedBlackTree<Key extends Comparable<Key>> {
 
 	/**
 	 * PreOrderVisit
+	 * 
 	 * @param n
 	 * @param v
 	 */
